668024d8adc63df67c08b6e4549511accb2bc1d0
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-07-29 17:17:27 +0200

    Refactor main function

--- text2excel/cli.py
+++ text2excel/cli.py
@@ -9,19 +9,33 @@ auto-detected.
 # By Fredrik Mellström <https://github.com/harkabeeparolus>
 # Based on https://gist.github.com/konrad/4154786 by Konrad Förstner
 import argparse
-import shutil
 import sys
-import textwrap
 
 from text2excel import convert
 from text2excel import version
+from text2excel.fmt import format_paragraphs
 
 
 def main(arguments=None):
     "Parse arguments and run text2excel from the command line."
-    if arguments is None:
-        arguments = sys.argv[1:]
+    parser = make_argument_parser()
+    args = parser.parse_args(arguments)
 
+    errors = 0
+    for input_file in args.files:
+        xlsxfile = convert.write_excel(input_file, convert_numbers=args.numbers)
+
+        if xlsxfile:
+            print(f"Saved to file: {xlsxfile}")
+        else:
+            print(f"error: Could not convert {input_file}")
+            errors += 1
+
+    return 1 if errors else 0
+
+
+def make_argument_parser():
+    "Create and return and argparse parser"
     epilog = "More information: https://github.com/harkabeeparolus/text2excel"
     parser = argparse.ArgumentParser(
         description=format_paragraphs(__doc__),
@@ -38,32 +52,7 @@ def main(arguments=None):
     parser.add_argument(
         "files", metavar="INPUT_FILE", nargs="+", help="text file(s) to convert"
     )
-    args = parser.parse_args(arguments)
-
-    errors = 0
-    for input_file in args.files:
-        xlsxfile = convert.write_excel(input_file, convert_numbers=args.numbers)
-
-        if xlsxfile:
-            print(f"Saved to file: {xlsxfile}")
-        else:
-            print(f"error: Could not convert {input_file}")
-            errors += 1
-
-    return 1 if errors else 0
-
-
-def format_paragraphs(input_text="", min_width=16):
-    "Dedent and text wrap paragraphs to terminal width."
-    width = max(min_width, shutil.get_terminal_size()[0] - 2)
-    # dedent and remove any initial newlines
-    text = textwrap.dedent(input_text).lstrip("\n")
-    # we return a final newline if the input has one
-    terminator = "\n" if text.endswith("\n") else ""
-    # remove trailing whitespace, and split by empty lines
-    paragraphs = "".join(text.rstrip()).split("\n\n")
-    # wrap and join paragraphs together with empty lines between
-    return "\n\n".join(textwrap.fill(p, width=width) for p in paragraphs) + terminator
+    return parser
 
 
 if __name__ == "__main__":
---
+++ text2excel/fmt.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python3
+"Text formatting utility, wrap text to screen width."
+# By Fredrik Mellström <https://github.com/harkabeeparolus>
+import fileinput
+import shutil
+import textwrap
+
+
+def format_paragraphs(input_text="", min_width=16):
+    "Dedent and text wrap paragraphs to terminal width."
+    width = max(min_width, shutil.get_terminal_size()[0] - 2)
+
+    # dedent and remove any initial newlines
+    text = textwrap.dedent(input_text).lstrip("\n")
+
+    # we return a final newline if the input has one
+    terminator = "\n" if text.endswith("\n") else ""
+
+    # remove trailing whitespace, and split by empty lines
+    paragraphs = "".join(text.rstrip()).split("\n\n")
+
+    # wrap and join paragraphs together with empty lines between
+    return "\n\n".join(textwrap.fill(p, width=width) for p in paragraphs) + terminator
+
+
+if __name__ == "__main__":
+    print(format_paragraphs("".join(fileinput.input())))
+
+
+# EOF
