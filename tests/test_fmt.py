013fa1d58523aa499115b01380291d8fe2478ec8
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-07-29 17:24:17 +0200

    Moved fmt test to separate file

---
+++ tests/test_fmt.py
@@ -0,0 +1,28 @@
+"pytest tests for fmt"
+from text2excel import fmt
+
+
+def test_format_paragraphs(monkeypatch):
+    "Try to dedent and reformat a paragraph."
+    lorem1 = """
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+    vehicula aliquam felis sed iaculis.
+
+    Integer vulputate dui vulputate metus pulvinar volutpat. Nullam
+    eu elementum libero.
+    """
+    lorem2 = (
+        "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\n"
+        "Vestibulum vehicula aliquam\nfelis sed iaculis.\n\nInteger "
+        "vulputate dui\nvulputate metus pulvinar\nvolutpat. Nullam "
+        "eu elementum\nlibero.\n"
+    )
+
+    with monkeypatch.context() as monkey:
+        monkey.setenv("COLUMNS", "32")
+        formatted = fmt.format_paragraphs(lorem1)
+    assert formatted == lorem2
+    assert fmt.format_paragraphs("foo  bar") == "foo  bar"
+
+
+# EOF
--- tests/test_text2excel.py
+++ tests/test_text2excel.py
@@ -55,27 +55,4 @@ def test_create_file():
         assert result.stat().st_size > 0
 
 
-def test_format_paragraphs(monkeypatch):
-    "Try to dedent and reformat a paragraph."
-    lorem1 = """
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
-    vehicula aliquam felis sed iaculis.
-
-    Integer vulputate dui vulputate metus pulvinar volutpat. Nullam
-    eu elementum libero.
-    """
-    lorem2 = (
-        "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\n"
-        "Vestibulum vehicula aliquam\nfelis sed iaculis.\n\nInteger "
-        "vulputate dui\nvulputate metus pulvinar\nvolutpat. Nullam "
-        "eu elementum\nlibero.\n"
-    )
-
-    with monkeypatch.context() as monkey:
-        monkey.setenv("COLUMNS", "32")
-        formatted = cli.format_paragraphs(lorem1)
-    assert formatted == lorem2
-    assert cli.format_paragraphs("foo  bar") == "foo  bar"
-
-
 # EOF
