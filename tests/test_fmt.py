37783ec0afe12a3c93337249d3f0fc72b171d6be
Fredrik Mellström <11281108+harkabeeparolus@users.noreply.github.com>
2020-11-25 15:58:35 +0100

    Improved paragraph text wrapping and tests

--- tests/test_fmt.py
+++ tests/test_fmt.py
@@ -4,25 +4,39 @@ from text2excel import fmt
 
 def test_format_paragraphs(monkeypatch):
     "Try to dedent and reformat a paragraph."
-    lorem1 = """
+
+    lorem_before = """
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
     vehicula aliquam felis sed iaculis.
 
     Integer vulputate dui vulputate metus pulvinar volutpat. Nullam
     eu elementum libero.
     """
-    lorem2 = (
-        "Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit.\n"
-        "Vestibulum vehicula aliquam\nfelis sed iaculis.\n\nInteger "
-        "vulputate dui\nvulputate metus pulvinar\nvolutpat. Nullam "
-        "eu elementum\nlibero.\n"
+
+    lorem_width35 = (
+        "Lorem ipsum dolor sit amet,\n"
+        "consectetur adipiscing elit.\n"
+        "Vestibulum vehicula aliquam\n"
+        "felis sed iaculis.\n"
+        "\n"
+        "Integer vulputate dui\n"
+        "vulputate metus pulvinar\n"
+        "volutpat. Nullam eu elementum\n"
+        "libero.\n"
     )
 
     with monkeypatch.context() as monkey:
         monkey.setenv("COLUMNS", "35")
-        formatted = fmt.format_paragraphs(lorem1)
-    assert formatted == lorem2
-    assert fmt.format_paragraphs("foo  bar") == "foo  bar"
+        formatted = fmt.format_paragraphs(lorem_before)
+
+    assert formatted == lorem_width35
+
+
+def test_newlines():
+    "Make sure we strip and reapply newlines correctly."
+
+    assert fmt.format_paragraphs("\n\nfoo  bar") == "foo  bar"
+    assert fmt.format_paragraphs("\n\nfoo bar baz\n\n") == "foo bar baz\n"
 
 
 # EOF
--- text2excel/fmt.py
+++ text2excel/fmt.py
@@ -2,26 +2,31 @@
 "Text formatting utility, wrap text to screen width."
 # By Fredrik Mellström <https://github.com/harkabeeparolus>
 import fileinput
+import re
 import shutil
 import textwrap
 
+NEWLINES_RE = re.compile(r"\n{2,}")
 
-def format_paragraphs(input_text="", min_width=16, margin=5):
+
+def format_paragraphs(input_text="", width=0, margin=5, min_width=16):
     "Dedent and text wrap paragraphs to terminal width."
-    columns, _ = shutil.get_terminal_size()
-    width = max(min_width, columns - margin)
+    if width <= 0:
+        columns, _ = shutil.get_terminal_size()
+        width = max(min_width, columns - margin)
 
     # dedent and remove any initial newlines
     text = textwrap.dedent(input_text).lstrip("\n")
 
-    # we return a final newline if the input has one
+    # we will return a final newline if the input has one
     terminator = "\n" if text.endswith("\n") else ""
 
     # remove trailing whitespace, and split by empty lines
-    paragraphs = "".join(text.rstrip()).split("\n\n")
+    paragraphs = NEWLINES_RE.split(text.rstrip())
 
     # wrap and join paragraphs together with empty lines between
-    return "\n\n".join(textwrap.fill(p, width=width) for p in paragraphs) + terminator
+    wrapper = textwrap.TextWrapper(width=width)
+    return "\n\n".join(wrapper.fill(p) for p in paragraphs) + terminator
 
 
 def merge_input_files(files=None):
